9bfb06caeee46066bccecf7bad0cb1cb8af652d6
= <=>
2014-02-17 13:31:32 -0500

    User now has an associated photo url for the first profile picture

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140217164750) do
+ActiveRecord::Schema.define(:version => 20140217183052) do
 
   create_table "profiles", :force => true do |t|
     t.string   "first_name",          :null => false
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(:version => 20140217164750) do
     t.datetime "created_at",      :null => false
     t.datetime "updated_at",      :null => false
     t.string   "session_token"
+    t.string   "photo_url"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email"
