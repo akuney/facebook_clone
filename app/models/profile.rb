9cb4036b2dffd32cacf8c85d2cc5acbbfa1ca982
= <=>
2014-02-17 13:48:17 -0500

    Adds appropriate presence validation to Profiles. Adds attr-accessible on the photo url.

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,7 +1,9 @@
 class Profile < ActiveRecord::Base
   attr_accessible :first_name, :last_name, :birth_month, :birth_day,
   :birth_year, :gender, :job, :employer, :current_city, :home_city,
-  :relationship_status, :interested_in, :about_me
+  :relationship_status, :interested_in, :about_me, :photo_url
 
   belongs_to :owner
+
+  validates :first_name, :last_name, :photo_url, presence: true
 end
