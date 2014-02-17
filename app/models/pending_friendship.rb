3405de55997cfa9f4808335e2829df76e6926f47
= <=>
2014-02-17 16:14:30 -0500

    Moved the helper methods to the model where they belong; for some reason I put them in the controller.

--- app/models/pending_friendship.rb
+++ app/models/pending_friendship.rb
@@ -1,3 +1,11 @@
 class PendingFriendship < ActiveRecord::Base
   attr_accessible :first_friend_id, :second_friend_id
+
+  def requests_sent(user)
+    return PendingFriendship.where?('first_friend_id = ?', user.id)
+  end
+
+  def requests_received(user)
+    return PendingFriendship.where?('second_friend_id = ?', user.id)
+  end
 end
