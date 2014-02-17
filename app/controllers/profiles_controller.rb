655104936b19c20cff45f1cb65b92f7748d756c7
= <=>
2014-02-17 12:03:06 -0500

    Starts new profile form. Puts authenticity token in. Please remember to do this.

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,2 +1,17 @@
 class ProfilesController < ApplicationController
+  def new
+    @user = current_user
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def show
+  end
 end
