c737ab495648e721f5e417ef049be3dba66afaea
= <=>
2014-02-17 11:39:25 -0500

    First commit. Authentication works.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FacebookClone::Application.config.secret_token = 'dbaa3a72be857a92b20c2c1e969a6998bd5e149c2e529f6855da33ed72791874ad9d13dc1c0a873d11759135a526f5b05dde2a1537091140d6045a5ae4d8e9a0'
