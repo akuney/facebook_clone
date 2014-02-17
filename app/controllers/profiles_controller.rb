94add5d600faf40d71230236df948d53a5f27a04
= <=>
2014-02-17 14:42:22 -0500

    Editing and new work correctly, go to proper places

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -6,7 +6,7 @@ class ProfilesController < ApplicationController
   def create
     @profile = Profile.new(params[:profile])
     @profile.save!
-    redirect_to profile_url(@profile)
+    redirect_to new_profile_url
   end
 
   def edit
@@ -14,7 +14,9 @@ class ProfilesController < ApplicationController
   end
 
   def update
+    @profile = Profile.find(params[:id])
     @profile.update_attributes(params[:profile])
+    redirect_to new_profile_url
   end
 
   def show
