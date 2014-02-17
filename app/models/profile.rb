797e86d674ba1cc210cb458c41e439f222560f81
= <=>
2014-02-17 13:51:56 -0500

    Puts tag in ERB that was causing bug. Adds more mass-assignability

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,7 +1,7 @@
 class Profile < ActiveRecord::Base
   attr_accessible :first_name, :last_name, :birth_month, :birth_day,
   :birth_year, :gender, :job, :employer, :current_city, :home_city,
-  :relationship_status, :interested_in, :about_me, :photo_url
+  :relationship_status, :interested_in, :about_me, :photo_url, :owner_id
 
   belongs_to :owner
 
