12ed4f60541ca3e81d98e5c44d6a9cd1e2317081
= <=>
2014-02-18 14:35:55 -0500

    Puts in the controller methods for Likes

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,2 +1,17 @@
 class LikesController < ApplicationController
+  def create
+    @like = Like.new(params[:like])
+    @like.save!
+    redirect_to :back
+  end
+
+  def destroy
+    @like = Like.find(params[:id])
+    @like.destroy
+    redirect_to :back
+  end
+
+  def index
+    @likes = Like.all
+  end
 end
