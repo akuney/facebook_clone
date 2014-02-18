9040b4788a62e28685c2f6e885d8beac40f442c0
= <=>
2014-02-18 16:04:03 -0500

    Q+A session about to start. The next task is to deal with the fact that comments have a recipient, and figure out where to adjust accordingly.

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,37 @@
 class CommentsController < ApplicationController
+  def new
+    @status = Status.new
+    @user = current_user
+  end
+
+  def index
+    @user = User.find(params[:user_id])
+    @statuses = @user.statuses
+  end
+
+  def show
+  end
+
+  def create
+    @status = Status.new(params[:status])
+    @status.save!
+    redirect_to user_statuses_url(current_user)
+  end
+
+  def destroy
+    @status = Status.find(params[:id])
+    @status.destroy
+    redirect_to user_statuses_url(current_user)
+  end
+
+  def edit
+    @status = Status.find(params[:id])
+  end
+
+  def update
+    @status = Status.find(params[:id])
+    @status.update_attributes(params[:status])
+    @status.save!
+    redirect_to user_statuses_url(current_user)
+  end
 end
