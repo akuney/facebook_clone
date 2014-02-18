1a6d9346b26de5bda0906828f082d2879f023353
= <=>
2014-02-18 13:35:29 -0500

    Writes the status creation method, and begins work on the new status method

--- app/controllers/statuses_controller.rb
+++ app/controllers/statuses_controller.rb
@@ -1,4 +1,8 @@
 class StatusesController < ApplicationController
+  def new
+    @user = current_user
+  end
+
   def index
     @user = User.find(params[:user_id])
     @statuses = @user.statuses
@@ -8,7 +12,9 @@ class StatusesController < ApplicationController
   end
 
   def create
-    render :index
+    @status = Status.new(params[:status])
+    @status.save!
+    redirect_to user_statuses_url(current_user)
   end
 
   def destroy
