20e8e003f4779edf3537279d4e84b88de14311ea
= <=>
2014-02-18 10:06:29 -0500

    Edits a small bug in the pending_friendships model (just forgetting to change wording).

--- app/models/pending_friendship.rb
+++ app/models/pending_friendship.rb
@@ -2,5 +2,5 @@ class PendingFriendship < ActiveRecord::Base
   attr_accessible :user_id, :pending_friend_id
 
   belongs_to :user
-  belongs_to :friend, :class_name => "User"
+  belongs_to :pending_friend, :class_name => "User"
 end
