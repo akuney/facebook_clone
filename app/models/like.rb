6b466dafa2603b6489e6bce7b960f3b028132e4a
= <=>
2014-02-18 14:51:13 -0500

    Adds helper method that lets us deal more easily with the Likes join table. May need to tweak id and type later.

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,6 +1,10 @@
 class Like < ActiveRecord::Base
-  attr_accessible :liker_id
+  attr_accessible :liker_id, :post_id, :post_type
 
   belongs_to :post, polymorphic: true
   belongs_to :liker, class_name: "User"
+
+  def self.find_by_liker_and_post(liker, post)
+    Like.where(liker_id: liker.id, post_id: post.id).first || []
+  end
 end
