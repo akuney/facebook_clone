97eb62a4fc241976b12a681c62512ee96e0d8328
= <=>
2014-02-19 14:26:41 -0500

    Finishes the messages index view. Adds a missing association. On the wish list: better way to extract user images. But I guess this might be Paperclip.

--- app/models/message_thread.rb
+++ app/models/message_thread.rb
@@ -5,6 +5,7 @@ class MessageThread < ActiveRecord::Base
 
   has_many :user_message_threads
   has_many :recipients, through: :user_message_threads, source: :recipient
+  has_many :messages, foreign_key: :thread_id
 
   def num_recipients
     self.recipients.count
