e333f414f3e132c69771d24e45db12c4dd8508ad
= <=>
2014-02-19 10:27:33 -0500

    Finishes skeleton for replies. Includes helper method for which url to go to: does it work?

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -4,17 +4,40 @@ class RepliesController < ApplicationController
     @reply = Reply.new(params[:reply])
     @reply.save!
 
-    if @reply.parent_type == "Comment"
-      redirect_to user_statuses_url(@reply.parent.recipient)
-    elsif @reply.parent_type == "Status"
-      redirect_to user_statuses_url(@reply.parent.author)
-    end
+    redirect_to appropriate_url
+  end
+
+  def edit
+    @reply = Reply.find(params[:id])
   end
 
   def update
+    @reply = Reply.find(params[:id])
+    @reply.update_attributes(params[:reply])
+    @reply.save!
+
+    redirect_to appropriate_url
   end
 
   def destroy
+    @reply = Reply.find(params[:id])
+
+    if @reply.parent_type == "Comment"
+      @url = user_statuses_url(@reply.parent.recipient)
+    elsif @reply.parent_type == "Status"
+      @url = user_statuses_url(@reply.parent.author)
+    end
+
+    redirect_to @url
   end
 
+  def appropriate_url
+    if @reply.parent_type == "Comment"
+      return user_statuses_url(@reply.parent.recipient)
+    elsif @reply.parent_type == "Status"
+      return user_statuses_url(@reply.parent.author)
+    else
+      return nil
+    end
+  end
 end
