c550bb2c2f9d97c9c28fb556bcb21d5601cab6db
= <=>
2014-02-19 13:01:45 -0500

    More associations

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,6 @@
 class Message < ActiveRecord::Base
-  # attr_accessible :title, :body
+  attr_accessible :creator_id, :thread_id, :title
+
+  belongs_to :creator, class_name: "User", foreign_key: "creator_id"
+  belongs_to :thread, class_name: "MessageThread", foreign_key: "thread_id"
 end
