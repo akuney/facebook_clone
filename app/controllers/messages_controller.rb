185d0410769e16be07e01b8c557af4a1690ed5c6
= <=>
2014-02-19 14:46:27 -0500

    Have new message form up and running; want to refactor into a partial for snappiness.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,9 +1,15 @@
 class MessagesController < ApplicationController
   def new
-
+    @message_thread = MessageThread.find(params[:message_thread_id])
   end
 
   def index
     @message_thread = MessageThread.find(params[:message_thread_id])
+  end
+
+  def create
+    @message = Message.new(params[:message])
+    @message.save!
+    redirect_to message_thread_messages(@message.thread_id)
   end
 end
