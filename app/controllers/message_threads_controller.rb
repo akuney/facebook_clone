81f8afd129a215687de64fd388a22e5762a33660
= <=>
2014-02-20 09:25:55 -0500

    Changes the name to message_thread_id everywhere I could find

--- app/controllers/message_threads_controller.rb
+++ app/controllers/message_threads_controller.rb
@@ -16,7 +16,7 @@ class MessageThreadsController < ApplicationController
     @message_thread.save!
 
     @message = Message.new(params[:message])
-    @message.thread_id = @message_thread.id
+    @message.message_thread_id = @message_thread.id
     @message.save!
 
     redirect_to message_threads_url
--- app/models/message.rb
+++ app/models/message.rb
@@ -2,5 +2,5 @@ class Message < ActiveRecord::Base
   attr_accessible :author_id, :thread_id, :text
 
   belongs_to :author, class_name: "User", foreign_key: "author_id"
-  belongs_to :thread, class_name: "MessageThread", foreign_key: "thread_id"
+  belongs_to :message_thread
 end
