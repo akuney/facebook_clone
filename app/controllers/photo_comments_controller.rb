fd1c32b6ea9f8bdb1fb6882c87d18bffc341ef64
= <=>
2014-02-20 11:03:17 -0500

    Photo original comment with editing seems to work

--- app/controllers/photo_comments_controller.rb
+++ app/controllers/photo_comments_controller.rb
@@ -1,11 +1,11 @@
 class PhotoCommentsController < ApplicationController
   def edit
-    @photo_comment = PhotoComment.find(params[:photo_comment])
+    @photo_comment = PhotoComment.find(params[:id])
   end
 
   def update
     @photo_comment = PhotoComment.find(params[:id])
-    @photo_comment.update_attributes
+    @photo_comment.update_attributes(params[:photo_comment])
     @photo_comment.save!
 
     redirect_to photo_url(@photo_comment.photo)
