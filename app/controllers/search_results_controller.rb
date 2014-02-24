95bcea0ac7a44c7856690f13545314fa8afb0a8e
App Academy Student <student@appacademy.io>
2014-02-24 15:17:43 -0500

    Searching works

--- app/controllers/search_results_controller.rb
+++ app/controllers/search_results_controller.rb
@@ -1,5 +1,14 @@
 class SearchResultsController < ApplicationController
   def index
+    query = params[:query]
+    results = PgSearch.multisearch(query)
+    @users = []
+
+    results.each do |result|
+      profile = Profile.find(result.searchable_id)
+      user = User.find(profile.owner_id)
+      @users << user
+    end
 
   end
 
