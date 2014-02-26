d81d6c26debdb987ddbd0e76345256cbaf5517fb
App Academy Student <student@appacademy.io>
2014-02-26 15:09:38 -0500

    Makes sure that a person only likes a post once

--- app/models/like.rb
+++ app/models/like.rb
@@ -4,6 +4,8 @@ class Like < ActiveRecord::Base
   belongs_to :post, polymorphic: true
   belongs_to :liker, class_name: "User", foreign_key: "liker_id"
 
+  validates_uniqueness_of :post_id, scope: [:liker_id]
+
   def self.find_by_liker_and_post(liker, post_id, post_type)
     Like.where(liker_id: liker.id, post_id: post_id, post_type: post_type).first || []
   end
