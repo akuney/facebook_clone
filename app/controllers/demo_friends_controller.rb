6154ad187e362096413fc4420dd193b9a69581c8
App Academy Student <student@appacademy.io>
2014-02-28 14:50:57 -0500

    Fixes a small bug with a user with id 6

--- app/controllers/demo_friends_controller.rb
+++ app/controllers/demo_friends_controller.rb
@@ -5,7 +5,7 @@ class DemoFriendsController < ApplicationController
     Friendship.create!(user_id: @user.id, friend_id: User.find(1).id)
     Friendship.create!(user_id: @user.id, friend_id: User.find(2).id)
     PendingFriendship.create!(user_id: @user.id, pending_friend_id: User.find(5).id)
-    PendingFriendship.create!(user_id: User.find(6).id, pending_friend_id: @user.id)
+    PendingFriendship.create!(user_id: User.find(3).id, pending_friend_id: @user.id)
 
     if request.xhr?
       render partial: 'demo_friends/all', locals: {user: @user}
